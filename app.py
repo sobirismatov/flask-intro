070dee5e549847ce92eb190cbb8c34a7cbc10b15
sobirismatov <name@uzcoder96@gmail.com>
2023-02-08 16:49:14 +0500

    flask

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
    return "<hi>Hello World!</hi>"
+@app.route("/about")
+def about():
+   return "<hi>About Page!</hi>"
 
 if __name__ == "__main__":
    app.run(port=None)
